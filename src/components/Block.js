214b0857ce3de44cc036f29f3c3ac99ec52fd498
romir17 <mathurromir@gmail.com>
2022-11-03 21:07:38 +0530

    block1

---
+++ src/components/Block.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function Block(){
+    return(
+    <footer>
+    <div>
+        <h1 class="text-green-500">DeCarbonUs</h1>
+        <h2>Understand Your Carbon Footprint</h2>
+    </div>
+    <div>
+        
+        <p>With our app, the user can easily monitor and analyze how their daily activities contribute to carbon emissions and how they can take measures to slowly reduce such contributing factors by taking simple steps. With the help of a graphical representation, the users get a thorough idea of its contributions to different 
+            categories such as eating preferences, travelling habits, fuel consumption, etc.</p>
+    
+    </div>
+    <div>
+        <h2>How it works ?</h2>
+        <ul>Just answer some basic questions</ul>
+        <ul>Get quick analysis</ul>
+        <ul>Understand your footprint VS UN </ul>
+        <ul>Get customised tracks</ul>
+        <ul>Start offsetting </ul>
+        <ul>Become a Champion</ul>
+    </div>
+    <div>
+        <h1>Answering Questions</h1>
+        <p>The app simply asks you questions about your regular activities to understand the carbon emissions per activity.
+            The activities taken under consideration are the primary parameters set by the UN standards hence no chance of error!!
+        </p>
+    
+        <h2>Quick Analysis</h2>
+        <p>The app provides a graphical representation of your carbon in respective day to day to activities to 
+            provide a better understanding of where you stand. The analysis shows where the maximum carbon emission has been done.
+            It also generates the report to give you an understanding how much you contribute with regard to the UN standards 
+        </p>
+
+        <h2>Customsied Tracks</h2>
+        <p>The app quickly provides you with tracks and tasks to reduce your carbon emissions in the category where the maximum contribution is done.
+            .The best part is that it rewards you with points and motivates you to push yourself.
+        </p>
+    </div>
+    </footer>
+
+
+        
+
+
+
+
+
+    
+    )
+}
+export default Block;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import Block from './components/Block';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />    
+    <Block />  
     </BrowserRouter>
   </React.StrictMode>
 );
